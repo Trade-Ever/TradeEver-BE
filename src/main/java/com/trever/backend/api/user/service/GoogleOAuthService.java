42fc368224e294ec20a6994be4b9b52ced6a8c1a
Kim Taemin <ruhokim1@naver.com>
2025-09-22 09:16:49 +0900

    [FEAT] ios 클라이언트 id 추가

---
+++ src/main/java/com/trever/backend/api/user/service/GoogleOAuthService.java
@@ -0,0 +1,38 @@
+package com.trever.backend.api.user.service;
+
+import com.google.api.client.googleapis.auth.oauth2.GoogleIdToken;
+import com.google.api.client.googleapis.auth.oauth2.GoogleIdTokenVerifier;
+import com.google.api.client.http.javanet.NetHttpTransport;
+import com.google.api.client.json.gson.GsonFactory;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+
+import java.io.IOException;
+import java.security.GeneralSecurityException;
+import java.util.Arrays;
+import java.util.Collections;
+@Slf4j
+@Service
+public class GoogleOAuthService {
+    private final GoogleIdTokenVerifier verifier;
+
+    public GoogleOAuthService(@Value("${google.oauth.web-client-id}") String webClientId, @Value("${google.oauth.ios-client-id}") String iosClientId) {
+        this.verifier = new GoogleIdTokenVerifier.Builder(new NetHttpTransport(), GsonFactory.getDefaultInstance())
+                .setAudience(Arrays.asList(webClientId, iosClientId))
+                .build();
+
+    }
+
+    public GoogleIdToken.Payload verifyIdToken(String idTokenString) {
+        try {
+            GoogleIdToken idToken = verifier.verify(idTokenString);
+            if (idToken == null) {
+                throw new IllegalArgumentException("Invalid Google ID token");
+            }
+            return idToken.getPayload();
+        } catch (GeneralSecurityException | IOException e) {
+            throw new IllegalArgumentException("Failed to verify ID token", e);
+        }
+    }
+}
